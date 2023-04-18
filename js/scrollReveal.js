c724eca03ca8d54fada14e840d91d5b23b02c774
Matheus Pessoa de Melo Gomes <matheuspdemelogomes@hotmail.com>
2023-04-17 21:17:55 -0300

    implementado efeitos de scrollReveal em alguns elementos da página e realizada alteração de css no formulário

---
+++ js/scrollReveal.js
@@ -0,0 +1,78 @@
+ScrollReveal().reveal('.hero-title', {
+    duration: 1000,
+    origin: 'top',
+    distance: '40%',
+    easing: 'ease-in',
+});
+
+
+ScrollReveal().reveal('.div-whatsapp-button', {
+    duration: 1000,
+    origin: 'right',
+    distance: '5%',
+    easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.div-instagram-button', {
+    duration: 1000,
+    origin: 'left',
+    distance: '5%',
+    easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.titles', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '30%',
+    easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.div-about-text', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '30%',
+    easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.card_dir', {
+    duration: 1500,
+    origin: 'dir',
+    distance: '100%',
+    easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.card_esq', {
+    duration: 1500,
+    origin: 'left',
+    distance: '100%',
+    easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.card_vantagem_container', {
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    rotate: {
+        x: 2000,
+        z: 200
+    }
+});
+
+
+ScrollReveal().reveal('.img_focus_antes', {
+    duration: 1000,
+    easing: 'ease-in',
+    rotate: {
+        x: -1000,
+        z: 20
+    }
+});
+
+ScrollReveal().reveal('.img_focus_depois', {
+    duration: 1000,
+    easing: 'ease-in',
+    rotate: {
+        x: -1000,
+        z: 20
+    }
+});
