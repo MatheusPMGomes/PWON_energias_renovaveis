00b34b4fc3cc4a530b3a0684091b1ec68fa0a6d9
Matheus Pessoa de Melo Gomes <matheuspdemelogomes@hotmail.com>
2023-04-18 12:40:56 -0300

    atualização no efeito scrollReveal

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,77 +1,17 @@
-ScrollReveal().reveal('.hero-title', {
-    duration: 1000,
-    origin: 'top',
-    distance: '40%',
-    easing: 'ease-in',
-});
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+ });
 
-
-ScrollReveal().reveal('.div-whatsapp-button', {
-    duration: 1000,
-    origin: 'right',
-    distance: '5%',
-    easing: 'ease-in',
-});
-
-ScrollReveal().reveal('.div-instagram-button', {
-    duration: 1000,
-    origin: 'left',
-    distance: '5%',
-    easing: 'ease-in',
-});
-
-ScrollReveal().reveal('.titles', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '30%',
-    easing: 'ease-in',
-});
-
-ScrollReveal().reveal('.div-about-text', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '30%',
-    easing: 'ease-in',
-});
-
-ScrollReveal().reveal('.card_dir', {
-    duration: 1500,
-    origin: 'dir',
-    distance: '25%',
-    easing: 'ease-in',
-});
-
-ScrollReveal().reveal('.card_esq', {
-    duration: 1500,
-    origin: 'left',
-    distance: '25%',
-    easing: 'ease-in',
-});
-
-ScrollReveal().reveal('.card_vantagem_container', {
-    duration: 2000,
-    easing: 'ease-in',
-    rotate: {
-        x: -1000,
-        z: 0
-    }
-});
-
-
-ScrollReveal().reveal('.img_focus_antes', {
-    duration: 2000,
-    easing: 'ease-in',
-    rotate: {
-        x: -1000,
-        z: 0
-    }
-});
-
-ScrollReveal().reveal('.img_focus_depois', {
-    duration: 2000,
-    easing: 'ease-in',
-    rotate: {
-        x: -1000,
-        z: 0
-    }
-});
+ ScrollReveal().reveal('.hero-title', {delay:500, origin: 'top'})
+ ScrollReveal().reveal('.div-whatsapp-button', {delay:500, origin: 'right'})
+ ScrollReveal().reveal('.div-instagram-button', {delay:500, origin: 'left'})
+ ScrollReveal().reveal('.titles', {origin: 'top'})
+ ScrollReveal().reveal('.div-about-text')
+ ScrollReveal().reveal('.card_dir', {origin: 'right'})
+ ScrollReveal().reveal('.card_esq', {origin: 'left'})
+ ScrollReveal().reveal('.card_vantagem_container', {rotate:{x:-1000,z:0}})
+ ScrollReveal().reveal('.img_focus_antes', {rotate:{x:-1000,z:0}})
+ ScrollReveal().reveal('.img_focus_depois', {rotate:{x:-1000,z:0}})
