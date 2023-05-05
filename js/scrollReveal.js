773b230769e1c0daf7099e90a53b6f32b30bb6d1
Matheus Pessoa de Melo Gomes <matheuspdemelogomes@hotmail.com>
2023-05-04 22:16:28 -0300

    correção do bug de posicionamento, para o responsivo, do hero-title e botões de redes sociais do hero

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -3,19 +3,19 @@ $(".div-whatsapp-button").delay(1000).animate({ opacity: 1 }, 1500);
 $(".div-instagram-button").delay(1000).animate({ opacity: 1 }, 1500);
 
 ScrollReveal({
-    reset: false,
-    distance: '60px',
-    duration: 2500,
-    delay: 400
- });
+  reset: false,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
 
- ScrollReveal().reveal('.hero-title', {delay:500, origin: 'top'})
- ScrollReveal().reveal('.div-whatsapp-button', {delay:500, origin: 'right'})
- ScrollReveal().reveal('.div-instagram-button', {delay:500, origin: 'left'})
- ScrollReveal().reveal('.titles', {origin: 'top'})
- ScrollReveal().reveal('.div-about-text')
- ScrollReveal().reveal('.card_dir', {origin: 'right'})
- ScrollReveal().reveal('.card_esq', {origin: 'left'})
- ScrollReveal().reveal('.card_vantagem_container', {rotate:{x:-1000,z:0}})
- ScrollReveal().reveal('.img_focus_antes', {rotate:{x:-1000,z:0}})
- ScrollReveal().reveal('.img_focus_depois', {rotate:{x:-1000,z:0}})
+ScrollReveal().reveal(".hero-title", { delay: 500, origin: "top" });
+ScrollReveal().reveal(".div-whatsapp-button", { delay: 500, origin: "right" });
+ScrollReveal().reveal(".div-instagram-button", { delay: 500, origin: "left" });
+ScrollReveal().reveal(".titles", { origin: "top" });
+ScrollReveal().reveal(".div-about-text");
+ScrollReveal().reveal(".card_dir", { origin: "right" });
+ScrollReveal().reveal(".card_esq", { origin: "left" });
+ScrollReveal().reveal(".card_vantagem_container", { rotate: { x: -1000, z: 0 } });
+ScrollReveal().reveal(".img_focus_antes", { rotate: { x: -1000, z: 0 } });
+ScrollReveal().reveal(".img_focus_depois", { rotate: { x: -1000, z: 0 } });
