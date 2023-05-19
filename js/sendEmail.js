3d8e30ab1fa6542f0c0a919cf875b7a70b190b4e
Matheus Pessoa de Melo Gomes <matheuspdemelogomes@hotmail.com>
2023-05-19 19:10:25 -0300

    alteração para tentar ajustar tempo de carregamento da imgda seção 'hero'

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,16 +1,14 @@
-const nome = document.getElementById('nome');
-const estado = document.getElementById('estado');
-const cidade = document.getElementById('cidade');
-const tipo_projeto = document.getElementById('tipo-projeto');
-const valor_luz = document.getElementById('valor-luz');
-const email = document.getElementById('email');
-const submit = document.getElementById('submit');
+const nome = document.getElementById("nome");
+const estado = document.getElementById("estado");
+const cidade = document.getElementById("cidade");
+const tipo_projeto = document.getElementById("tipo-projeto");
+const valor_luz = document.getElementById("valor-luz");
+const email = document.getElementById("email");
+const submit = document.getElementById("submit");
 
-
-
-submit.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let ebody = `
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let ebody = `
     <h1>Nome: </h1>${nome.value}
     <br>
     <h1>Estado: </h1>${estado.value}
@@ -24,26 +22,20 @@ submit.addEventListener('submit',(e)=>{
     <h1>Email: </h1>${email.value}
     `;
 
-    Email.send({
-        SecureToken : "4745527e-3b45-4988-aefd-aa4b0c771acc", //add your token here
-        To : 'orcamentos@pwon.com.br', 
-        From : "orcamentos@pwon.com.br",
-        Subject : "Orçamento do site PWON",
-        Body : ebody
-    }).then(
-      message => alert(message),
-      apagarDadosFormulario()
-    );
-
-    
+  Email.send({
+    SecureToken: "4745527e-3b45-4988-aefd-aa4b0c771acc", //add your token here
+    To: "orcamentos@pwon.com.br",
+    From: "orcamentos@pwon.com.br",
+    Subject: "Orçamento do site PWON",
+    Body: ebody,
+  }).then((message) => alert(message), apagarDadosFormulario());
 });
 
-function apagarDadosFormulario(){
-    nome.value = '';
-    estado.value = '';
-    cidade.value = '';
-    tipo_projeto.value = '';
-    valor_luz.value = '';
-    email.value = '';
+function apagarDadosFormulario() {
+  nome.value = "";
+  estado.value = "";
+  cidade.value = "";
+  tipo_projeto.value = "";
+  valor_luz.value = "";
+  email.value = "";
 }
-
